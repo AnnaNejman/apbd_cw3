75ce15d481e60240f5cff080d2fde551391d7af8
AnnaNejman <s28181@pjwstk.edu.pl>
2025-03-25 19:01:21 +0100

    .

--- Cwiczenia3/Program.cs
+++ Cwiczenia3/Program.cs
@@ -41,7 +41,6 @@ public interface IsHazardNotifier
 }
 public class LiquidContainer : Container, IsHazardNotifier
 {
-    private IsHazardNotifier _isHazardNotifierImplementation;
     public bool IsHazardNotified { get; set; }
     public LiquidContainer(double wysokosc, double wagaWlasna, double glebokosc, double maxLadownosc, bool isHazardNotified) 
         : base("L", wysokosc, wagaWlasna, glebokosc, maxLadownosc)
@@ -52,30 +51,27 @@ public class LiquidContainer : Container, IsHazardNotifier
     public override void LoadContainer(double masa)
     {
         double procentWypelnienia = IsHazardNotified ? MaxLadownosc * 0.5 : MaxLadownosc * 0.9;
-        if (masa > procentWypelnienia-MasaLadunku)
+        if (masa > procentWypelnienia - MasaLadunku)
         {
-            Notify($"ALERT: {NumerSeryjny}");
-            
+            Console.WriteLine($"ALERT: {NumerSeryjny}");
         }
         base.LoadContainer(masa);
     }
     
     public void Notify(string msg)
     {
-        Notify(msg);
-        throw new InvalidOperationException("Przeladowanie: Przekroczono limit masy kontenera!");
+
+        throw new InvalidOperationException(msg+ "Przeladowanie: Przekroczono limit masy kontenera!");
     }
 }
 
 public class GazContainer : Container, IsHazardNotifier
 {
     public double Cisnienie { get; set; }
-    public bool IsHazardNotified { get; set; }
     
-    public GazContainer(double cisnienie, double wysokosc, double wagaWlasna, double glebokosc, double maxLadownosc, bool isHazardNotified) 
+    public GazContainer(double cisnienie, double wysokosc, double wagaWlasna, double glebokosc, double maxLadownosc) 
         : base("G", wysokosc, wagaWlasna, glebokosc, maxLadownosc)
     {
-        IsHazardNotified = isHazardNotified;
         Cisnienie = cisnienie;
     }
     public override void EmptyContainer()
@@ -84,8 +80,7 @@ public class GazContainer : Container, IsHazardNotifier
     }
     public void Notify(string msg)
     {
-        Notify(msg);
-        throw new InvalidOperationException("Przeladowanie: Przekroczono limit masy kontenera!");
+        throw new InvalidOperationException(msg + " Przeladowanie: Przekroczono limit masy kontenera!");
     }
     public override void LoadContainer(double masa)
     {
@@ -135,7 +130,125 @@ public class RefrigeratedContainer : Container, IsHazardNotifier
 
     public void Notify(string msg)
     {
-        Notify(msg);
-        throw new InvalidOperationException("Przeladowanie: Przekroczono limit masy kontenera!");
+        throw new InvalidOperationException(msg);
     }
 }
+
+public class ContainerShip
+{
+    public string Name;
+    private List<Container> Containers { get; set; }
+    public double Speed { get; set; }
+    public int MaxContainersNumber { get; set; }
+    public double MaxWeight { get; set; }
+
+    public ContainerShip(string name, double speed, int maxContainersNumber, double maxWeight)
+    {
+        Name = name;
+        Speed = speed;
+        MaxContainersNumber = maxContainersNumber;
+        MaxWeight = maxWeight;
+        Containers = new List<Container>();
+    }
+
+    public void AddContainer(Container container)
+    {
+        if (Containers.Count >= MaxContainersNumber)
+        {
+            throw new Exception("Kontenerowiec jest juz pelny!");
+        }
+        
+        if (GetCurrentLoad()+container.MasaLadunku+container.WagaWlasna > MaxWeight)
+        {
+            throw new Exception("Masa kontenera jest zbyt duza zeby zaladowac go na ten kontenerowiec!");
+        }
+        Containers.Add(container);
+    }
+    public void AddContainer(List<Container> containers)
+    {
+        for (int i = 0; i < containers.Count; i++)
+        {
+            AddContainer(containers[i]);
+        }
+    }
+
+    public void RemoveContainer(string serialNumber)
+    {
+        var element = Containers.RemoveAll(c => c.NumerSeryjny == serialNumber);
+        if (element == 0)
+        {
+            throw new Exception($"Nie znaleziono elementu z numerem seryjnym: {serialNumber}");
+        }
+    }
+    public void ReplaceContainer(string serialNumber, Container newContainer)
+    {
+        RemoveContainer(serialNumber);
+        AddContainer(newContainer);
+    }
+
+    public void MoveContainer(Container container,ContainerShip newContainerShip)
+    {
+        RemoveContainer(container.NumerSeryjny);
+        newContainerShip.AddContainer(container);
+    }
+    public void PrintContainerInfo(string serialNumber)
+    {
+        var container = Containers.Find(c => c.NumerSeryjny == serialNumber);
+        if (container != null)
+        {
+            Console.WriteLine($"Kontener: {container.NumerSeryjny}, Zaladowano: {container.MasaLadunku} kg, Max masa ładunku: " +
+                              $"{container.MaxLadownosc} kg, Masa własna: {container.WagaWlasna} kg, Wysokość: {container.Wysokosc} cm, Głębokość: {container.Glebokosc} cm");
+        }
+        else
+        {
+            Console.WriteLine("Nie znaleziono tego kontenera.");
+        }
+    }
+    public void PrintShipInfo()
+    {
+        Console.WriteLine($"Kontenerowiec: {Name}, Max waga ładunku: {MaxWeight} ton, Max liczba kontenerów: {MaxContainersNumber}, Załaodowano: " +
+                          $"{GetCurrentLoad()} ton, Liczba kontenerów załadowanych: {Containers.Count}");
+    }
+
+    public double GetCurrentLoad()
+    {
+        return Containers.Sum(c => c.MasaLadunku + c.WagaWlasna);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        ContainerShip ship1 = new ContainerShip("s1", 50, 10, 100);
+        ContainerShip ship2 = new ContainerShip("s2", 25, 12, 200);
+
+        Container c1 = new LiquidContainer(3, 1, 3, 50, false);
+        Container c2 = new GazContainer(100,5,1,5,60);
+        Container c3 = new RefrigeratedContainer(20, 1, 5, 5, "Bananas", 20);
+        
+        c1.LoadContainer(6);
+        c2.LoadContainer(2);
+
+        ship1.AddContainer(c1);
+        ship2.AddContainer(c2);
+        
+        List<Container> containers = new List<Container>();
+        containers.Add(c2);
+        containers.Add(c3);
+        containers.Add(c1);
+        
+        ship1.PrintShipInfo();
+        ship1.ReplaceContainer(c1.NumerSeryjny, c2);
+        
+        ship1.MoveContainer(c2,ship2);
+        ship2.PrintContainerInfo(c1.NumerSeryjny);
+        
+        ship1.AddContainer(containers);
+        
+        ship1.PrintShipInfo();
+        ship2.PrintShipInfo();
+        
+    }
+}
+
+
