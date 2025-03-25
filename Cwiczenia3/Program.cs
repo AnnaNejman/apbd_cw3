49a90f7ffccef04344dd9fa8bc5e2338c0a21a8a
AnnaNejman <s28181@pjwstk.edu.pl>
2025-03-25 14:24:40 +0100

    Create Program.cs
    Kontener dodany

---
+++ Cwiczenia3/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+public class Container
+{
+    private static int counter = 0;
+    public double MasaLadunku { get; set; }
+    public double Wysokosc { get; set; }
+    public double WagaWlasna { get; set; }
+    public double Glebokosc { get; set; }
+    public String NumerSeryjny { get; set; }
+    public double MaxLadownosc { get; set; }
+
+    public Container(string type, double wysokosc, double wagaWlasna, double glebokosc, double maxLadownosc)
+    {
+        int newNumber = ++counter;
+        MasaLadunku = 0;
+        Wysokosc = wysokosc;
+        WagaWlasna = wagaWlasna;
+        Glebokosc = glebokosc;
+        MaxLadownosc = maxLadownosc;
+        NumerSeryjny = $"KON-{type}-{newNumber}";
+    }
+
+    public void EmptyContainer()
+    {
+        MasaLadunku = 0;
+    }
+
+    public void LoadContainer(double masa)
+    {
+        if (MasaLadunku + masa > MaxLadownosc)
+        {
+            throw new InvalidOperationException("Przekroczono maksymalną ładowność kontenera!");
+        }
+        MasaLadunku += masa;
+    }
+    
+}
